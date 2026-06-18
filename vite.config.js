a96fcf402286db59cc1cfdcdc2a83a436c8da3e8
Abraham10VelGlez <usbrifa@gmail.com>
2026-06-18 11:09:12 -0600

    actualizacion

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //require('daisyui'),
+  server: {//configuraciones para desarrollo en docker-compose.yml
+    watch: {//configuraciones para desarrollo en docker-compose.yml
+      usePolling: true,//configuraciones para desarrollo en docker-compose.yml
+    },//configuraciones para desarrollo en docker-compose.yml
+    host: '0.0.0.0',//configuraciones para desarrollo en docker-compose.yml
+    port: 5173//configuraciones para desarrollo en docker-compose.yml
+  },//configuraciones para desarrollo en docker-compose.yml
+})
